ec9b8cedb68115ac9797e501f47e1502c8c4a117
James Clark <James.Clark@sophos.com>
2017-07-16 10:17:03 +0100

    Refactored to work

--- spring-boot-samples/spring-boot-sample-web-jsp/src/main/java/sample/jsp/SampleWebJspApplication.java
+++ spring-boot-samples/spring-boot-sample-web-jsp/src/main/java/sample/jsp/SampleWebJspApplication.java
@@ -19,7 +19,7 @@ package sample.jsp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SampleWebJspApplication extends SpringBootServletInitializer {
